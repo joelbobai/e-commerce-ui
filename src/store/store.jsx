d5a9587bdf76322fa602feb3a956aa7d2a6e2265
joelbobai <90658305+joelbobai@users.noreply.github.com>
2023-08-10 00:45:02 +0100

    Update store.jsx

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -4,7 +4,10 @@ export const useAuthStore = create((set) => ({
   auth: {
     user: [],
     isLoggedIn: false,
+    authToken: "",
   },
+  setAuthToken: (str) =>
+    set((state) => ({ auth: { ...state.auth, authToken: str } })),
   setUser: (obj) => set((state) => ({ auth: { ...state.auth, user: obj } })),
   setIsLoggedIn: (bool) =>
     set((state) => ({ auth: { ...state.auth, isLoggedIn: bool } })),
